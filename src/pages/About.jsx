3bdcb8d75842223cefbb3dbf265e6bd5bc83d417
Meen Philip <meenphilip@gmail.com>
2023-01-12 16:16:35 +0300

    About page.

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, rerum
+        modi. Commodi laudantium, harum obcaecati, ex molestias laborum quas
+        maxime, explicabo ipsa cupiditate ipsam facere nemo praesentium aliquid
+        repellat. Illo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Illum, rerum modi. Commodi laudantium, harum obcaecati, ex molestias
+        laborum quas maxime, explicabo ipsa cupiditate ipsam facere nemo
+        praesentium aliquid repellat. Illo?
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </>
+  );
+}
+
+export default About;
